76bc34db4c0e1dbbd9f2746c523e3f15374e4864
fedos11 <fiodorpetrow2@gmail.com>
2024-03-12 18:37:20 +0300

    1 Lab.

---
+++ Lab1.2/data_analysis_lab.py
@@ -0,0 +1,20 @@
+from matplotlib import pyplot
+from openpyxl import load_workbook
+
+def extract_value(cell):
+    return cell.value
+
+
+wb = load_workbook('data_analysis_lab.xlsx')
+
+a_column = list(map(extract_value, wb['Data']['A'][1:]))
+c_column = list(map(extract_value, wb['Data']['C'][1:]))
+d_column = list(map(extract_value, wb['Data']['D'][1:]))
+
+pyplot.plot(a_column, c_column, label='График 1')
+pyplot.plot(a_column, d_column, label='График 2')
+
+pyplot.xlabel('Год')
+pyplot.ylabel('Относительная температура и активность солнца')
+pyplot.legend(loc='upper left')
+pyplot.show()
